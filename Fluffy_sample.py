2cb1bee1112eb7479a02188155dcf7cf101cb029
Lama1337 <lamaen1337@hotmail.com>
2020-02-05 16:42:11 +0100

    Create Fluffy_sample.py

---
+++ Fluffy_sample.py
@@ -0,0 +1,8 @@
+#05.02.2020 - Forsøk på å lære noe nytt
+#Denne blir bare brukt for å teste ut diverse funksjoner i python
+
+#Etterspør credentials
+Fornavn = input("Fornavn: ")
+Etternavn = input("Etternavn: ")
+Mobilnummer = input("Mobil: ")
+Epost = input("Epost: ")
