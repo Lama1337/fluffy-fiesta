bcefdfdd5a42aa6eda526c6fed62ac963b5f0919
Lama1337 <lamaen1337@hotmail.com>
2020-02-05 18:29:47 +0100

    Create Pong.py

---
+++ Pong.py
@@ -0,0 +1,30 @@
+##Mitt forsøk på å kode pong##
+##05.02.2020##
+
+#requisites
+#upgrade pip = py -m pip install --upgrade pip
+#install pygame_module = py -m pip install -U pygame --user
+
+#Import av pygamemodul når jeg skjønner hvordan det gjøres....
+import pygame
+pygame.init()
+
+#Tegner spillbrett ,(Pygame bruker noe som heter surface du kan kun ha 1 av den)
+gameDisplay = pygame.display.set_mode((1024,768))
+
+
+#Tegner spillere + ball (more surface code i assume)
+
+
+
+Playernames
+Player1 = input("Player1: ")
+Player2 = input("Player2: ")
+
+#Definerer kontrollinput
+
+Player1.up = "arrow_up"
+Player1.down = #
+
+Player2.up = "w"
+Player2.down = "s"
